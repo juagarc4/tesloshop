1b3651f67a0006211bd106634150bc0847f47c49
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-12 20:01:37 +0200

    Remove waste files

--- .history/pages/index_20220711205421.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-  return <h1></h1>
-}
-
-export default Home
--- .history/pages/index_20220711205434.tsx
+++
@@ -1,7 +0,0 @@
-import type { NextPage } from 'next'
-
-const Home: NextPage = () => {
-  return <h1>Teslo - Shop</h1>
-}
-
-export default Home
