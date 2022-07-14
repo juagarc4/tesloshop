221c7e26b6e4a468989c4b161a5e01cd57b6c13d
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-14 18:01:19 +0200

    Product page 2.Product slideshow

--- pages/product/slug.tsx
+++ pages/product/slug.tsx
@@ -1,6 +1,7 @@
 import { Box, Button, Chip, Grid, Typography } from '@mui/material'
 import { ShopLayout } from 'components/layouts'
 import { initialData } from 'database/products'
+import { ProductSlideshow } from '../../components/products/ProductSlideshow'
 
 const product = initialData.products[0]
 const ProductPage = () => {
@@ -8,7 +9,7 @@ const ProductPage = () => {
     <ShopLayout title={product.title} pageDescription={product.description}>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={7}>
-          {/* slideshow */}
+          <ProductSlideshow images={product.images} />
         </Grid>
         <Grid item xs={12} sm={5}>
           <Box display='flex' flexDirection='column'>
