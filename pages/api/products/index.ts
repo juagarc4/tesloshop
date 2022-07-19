63edce0deccde5c305cfae36b58e2d69af4c008a
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-19 16:59:54 +0200

    RestFul 1. Products endpoint

---
+++ pages/api/products/index.ts
@@ -0,0 +1,23 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db } from 'database'
+import { IProduct } from 'interfaces'
+import { Product } from 'models'
+
+type Data = { message: string } | IProduct[]
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case 'GET':
+      return getProducts(req, res)
+
+    default:
+      return res.status(400).json({ message: 'Bad request' })
+  }
+}
+const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  await db.connect()
+  const products = await Product.find().select('title images price size slug inStock -_id').lean()
+  await db.disconnect()
+
+  return res.status(200).json(products)
+}
