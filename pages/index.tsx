5ecf53b82202ff9de0bf2ebd37422f98a83477b6
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-20 21:20:31 +0200

    Add SWR 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,16 +4,24 @@ import { seedData } from 'database/products'
 import { ShopLayout } from 'components/layouts/ShopLayout'
 import { ProductList } from 'components/products'
 
-const Home: NextPage = () => {
+import useSWR from 'swr'
+const fetcher = (...args: [key: string]) => fetch(...args).then((res) => res.json())
+
+const HomePage: NextPage = () => {
+  const { data, error } = useSWR('/api/products', fetcher)
+
+  if (error) return <div>failed to load</div>
+  if (!data) return <div>loading...</div>
+
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Find the best prodcut of Teslo here'}>
       <Typography variant='h1'>Shop</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>
         All Products
       </Typography>
-      <ProductList products={seedData.products as any} />
+      <ProductList products={data} />
     </ShopLayout>
   )
 }
 
-export default Home
+export default HomePage
