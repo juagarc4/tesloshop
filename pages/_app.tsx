02b94576c232e0f48f13abfdc5ac23e747535c90
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-21 13:54:31 +0200

    Add SWR 2. UseGlobal SWR

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from 'next/app'
 import { CssBaseline, ThemeProvider } from '@mui/material'
+import { SWRConfig } from 'swr'
 
 import { lightTheme } from 'themes'
-import 'styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={lightTheme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <SWRConfig
+      value={{
+        fetcher: (resource, init) => fetch(resource, init).then((res) => res.json()),
+      }}
+    >
+      <ThemeProvider theme={lightTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
   )
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
 import type { NextPage } from 'next'
 import { Typography } from '@mui/material'
-import { seedData } from 'database/products'
 import { ShopLayout } from 'components/layouts/ShopLayout'
 import { ProductList } from 'components/products'
-
-import useSWR from 'swr'
-const fetcher = (...args: [key: string]) => fetch(...args).then((res) => res.json())
+import { useProducts } from 'hooks'
 
 const HomePage: NextPage = () => {
-  const { data, error } = useSWR('/api/products', fetcher)
-
-  if (error) return <div>failed to load</div>
-  if (!data) return <div>loading...</div>
+  const { products, isLoading } = useProducts('/products')
 
   return (
     <ShopLayout title={'Teslo-Shop - Home'} pageDescription={'Find the best prodcut of Teslo here'}>
@@ -19,7 +13,7 @@ const HomePage: NextPage = () => {
       <Typography variant='h2' sx={{ mb: 1 }}>
         All Products
       </Typography>
-      <ProductList products={data} />
+      {isLoading ? <h1>Loading...</h1> : <ProductList products={products} />}
     </ShopLayout>
   )
 }
