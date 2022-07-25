ee24a58856df73740830113f62fe0c903b630459
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-25 20:23:27 +0200

    Calculate order summary values

--- context/cart/CartProvider.tsx
+++ context/cart/CartProvider.tsx
@@ -31,6 +31,21 @@ export const CartProvider: FC<Props> = ({ children }) => {
     Cookie.set('cart', JSON.stringify(state.cart))
   }, [state.cart])
 
+  useEffect(() => {
+    const numberOfItems = state.cart.reduce((prev, current) => current.quantity + prev, 0)
+    const subTotal = state.cart.reduce((prev, current) => current.price * current.quantity + prev, 0)
+    const taxes = subTotal * Number(process.env.NEXT_PUBLIC_TAX_RATE || 0)
+    const total = subTotal + taxes
+
+    const orderSummary = {
+      numberOfItems,
+      subTotal,
+      taxes,
+      total,
+    }
+    console.log(orderSummary)
+  }, [state.cart])
+
   const addProductToCart = (product: ICartProduct) => {
     // Solution: Step 1 (Not practical) => Duplicated products in cart can happen
     // dispatch({ type: '[Cart] - Add product', payload: product })
