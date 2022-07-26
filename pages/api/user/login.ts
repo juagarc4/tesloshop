f6fddf7f517c1a1c0d9764e263725820234a1b4e
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-26 17:37:32 +0200

    Login Endpoint

---
+++ pages/api/user/login.ts
@@ -0,0 +1,52 @@
+import { db } from 'database'
+import { User } from 'models'
+import type { NextApiRequest, NextApiResponse } from 'next'
+import bcrypt from 'bcryptjs'
+
+type Data =
+  | {
+      message: string
+    }
+  | {
+      token: string
+      user: {
+        email: string
+        name: string
+        role: string
+      }
+    }
+
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  switch (req.method) {
+    case 'POST':
+      return loginUser(req, res)
+
+    default:
+      res.status(400).json({ message: 'Bad request' })
+      break
+  }
+}
+const loginUser = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { email = '', password = '' } = req.body
+  await db.connect()
+  const user = await User.findOne({ email })
+  await db.disconnect()
+
+  if (!user) {
+    return res.status(400).json({ message: 'User not valid - EMAIL' })
+  }
+
+  if (!bcrypt.compareSync(password, user.password!)) {
+    return res.status(400).json({ message: 'User not valid - PASS' })
+  }
+
+  const { role, name } = user
+  return res.status(200).json({
+    token: '', // jwt
+    user: {
+      email,
+      role,
+      name,
+    },
+  })
+}
