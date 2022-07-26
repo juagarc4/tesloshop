747ba6d82aed6186b20ed587d0a3bacae51746ca
Raul Garcia <raulgarciacanet@gmail.com>
2022-07-26 15:52:23 +0200

    User model and User Interface

---
+++ interfaces/user.ts
@@ -0,0 +1,11 @@
+export interface IUser {
+  _id: string
+  name: string
+  email: string
+  password?: string
+  role: IRole
+  createdAt?: string
+  updatedAt?: string
+}
+
+export type IRole = 'admin' | 'client'
