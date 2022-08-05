a9c209da9aa7b5c57ce5f55d53078969fba15d7a
Raul Garcia <raulgarciacanet@gmail.com>
2022-08-05 16:43:51 +0200

    Remove cookies on logout

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -87,7 +87,16 @@ export const AuthProvider: FC<Props> = ({ children }) => {
   const logout = () => {
     Cookies.remove('token')
     Cookies.remove('cart')
+    Cookies.remove('firstName')
+    Cookies.remove('lastName')
+    Cookies.remove('address')
+    Cookies.remove('address2')
+    Cookies.remove('postalCode')
+    Cookies.remove('city')
+    Cookies.remove('country')
+    Cookies.remove('phone')
     router.reload()
+    router.replace('/')
   }
   return <AuthContext.Provider value={{ ...state, loginUser, registerUser, logout }}>{children}</AuthContext.Provider>
 }
