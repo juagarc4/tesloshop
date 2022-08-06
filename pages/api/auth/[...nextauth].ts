fbb1291196addec145bbbc2fa12efee5fc015dcf
Raul Garcia <raulgarciacanet@gmail.com>
2022-08-06 16:15:57 +0200

    NextAuth 4: INtegrate our custom login in nextauth

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,12 +1,25 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+import Credentials from 'next-auth/providers/credentials'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
+    Credentials({
+      name: 'Custom Login',
+      credentials: {
+        email: { label: 'E-Mail', type: 'email', placeholder: 'Your E-Mail' },
+        password: { label: 'Password', type: 'password', placeholder: 'Your password' },
+      },
+      async authorize(credentials) {
+        console.log(credentials)
+        // TODO: Validate against DB
+        return { name: 'Raul', email: 'raulgarciacanet@gmail.com', role: 'admin' }
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
     }),
-    // ...add more providers here
   ],
 })
