503f98d96eb4cbf0d708464a8f66799dcc3bf8be
Raul Garcia <raulgarciacanet@gmail.com>
2022-08-16 11:18:52 +0200

    Admin dashboard 4: Validation in Miidleware

--- middleware.ts
+++ middleware.ts
@@ -3,11 +3,25 @@ import { getToken } from 'next-auth/jwt'
 
 // This function can be marked `async` if using `await` inside
 export async function middleware(req: NextRequest | any) {
-  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
-  if (!session) {
-    return NextResponse.redirect(`http://localhost:3000/auth/login?p=${config.matcher}`)
+  const session: any = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
+  const url = req.nextUrl.clone()
+
+  if (req.nextUrl.pathname.startsWith(config.matcher[0])) {
+    if (!session) {
+      url.pathname = '/auth/login'
+      url.searchParams.set('p', config.matcher[0])
+      return NextResponse.redirect(url)
+    }
   }
+  if (req.nextUrl.pathname.startsWith(config.matcher[1])) {
+    const validRoles = ['admin', 'super-user', 'SEO']
+    if (!session || !validRoles.includes(session.user.role)) {
+      url.pathname = '/'
+      return NextResponse.redirect(url)
+    }
+  }
+  return NextResponse.next()
 }
 export const config = {
-  matcher: '/checkout/address',
+  matcher: ['/checkout/address', '/admin'],
 }
