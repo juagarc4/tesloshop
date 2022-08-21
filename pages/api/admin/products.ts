2133f49f75bb3042b6a9f3565da1d56ea0a837b9
Raul Garcia <raulgarciacanet@gmail.com>
2022-08-21 10:09:13 +0200

    Products 4: Edit form 4 - Endpoint to update product

--- pages/api/admin/products.ts
+++ pages/api/admin/products.ts
@@ -1,13 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { isValidObjectId } from 'mongoose'
 import { db } from 'database'
 import { IProduct } from 'interfaces'
 import { Product } from 'models'
-import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data =
   | {
       message: string
     }
   | IProduct[]
+  | IProduct
 
 export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
   switch (req.method) {
@@ -15,6 +17,7 @@ export default function handler(req: NextApiRequest, res: NextApiResponse<Data>)
       return getProducts(req, res)
 
     case 'PUT':
+      return updateProdct(req, res)
     case 'POST':
     default:
       return res.status(400).json({ message: 'Bad request' })
@@ -27,4 +30,33 @@ const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
 
   return res.status(200).json(products)
   // TODO: Update images
+}
+const updateProdct = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+  const { _id = '', images = [] } = req.body as IProduct
+
+  if (!isValidObjectId(_id)) {
+    return res.status(400).json({ message: 'Product id not valid' })
+  }
+
+  if (images.length <= 2) {
+    return res.status(400).json({ message: 'At least 2 images are needed' })
+  }
+
+  // TODO: Full urls for images
+
+  try {
+    await db.connect()
+    const product = await Product.findById(_id)
+    if (!product) {
+      await db.disconnect()
+      return res.status(400).json({ message: 'Product does not exist' })
+    }
+    //TODO: Remove images in Cloudinary
+    await product.update(req.body)
+    await db.disconnect()
+    return res.status(200).json(product)
+  } catch (error) {
+    await db.disconnect()
+    return res.status(400).json({ message: 'Unhandled error: Review server console.' })
+  }
 }
