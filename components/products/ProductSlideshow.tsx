ee35f63a56fba9d5d3da1bd2e0c28b882a2fb8c4
Raul Garcia <raulgarciacanet@gmail.com>
2022-09-16 22:59:41 +0200

    Fix deployment on Heroku 6: Type Error

--- components/products/ProductSlideshow.tsx
+++ components/products/ProductSlideshow.tsx
@@ -1,7 +1,7 @@
 import { FC } from 'react'
 import { Slide } from 'react-slideshow-image'
 import 'react-slideshow-image/dist/styles.css'
-import styles from './ProductSlideShow.module.css'
+import styles from './ProductSlideshow.module.css'
 
 interface Props {
   images: string[]
